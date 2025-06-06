93e1176925ccf7226099d21cc227ed307a3f0950
yebis0942 <yebis0942@gmail.com>
2025-06-06 23:46:40 +0900

    スポンサー一覧を追加

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,17 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 
 const staff = defineCollection({
   loader: glob({ pattern: "*.json", base: "src/data/staff" }),
 });
 
-export const collections = { staff };
+const sponsors = defineCollection({
+  loader: file("src/data/sponsors/data.json"),
+  schema: z.object({
+    name: z.string(),
+    image: z.string(),
+  }),
+});
+
+export const collections = { staff, sponsors };
